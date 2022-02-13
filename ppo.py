9760156143da2edcc9b9325016fb7321598476b8
Alonsoff37 <alonsorizo375@gmail.com>
2022-02-13 15:03:29 -0600

    Step 6

--- ppo.py
+++ ppo.py
@@ -1,5 +1,9 @@
 from networks import FeedForwardNN
 
+import torch
+from torch.distributions import MultivariateNormal
+from torch.optim import Adam
+
 class PPO:
     def __init__(self, env):
         #Informacion del environment
@@ -12,6 +16,7 @@ class PPO:
         self.actor = FeedForwardNN(self.obs_dim, self.act_dim)
         self.critic = FeedForwardNN(self.obs_dim, 1)
         self._init_hyperparameters()
+        self.actor_optim = Adam(self.actor.parameters(), lr = self.lr)
 
         #TODO change fill_value
         self.cov_var = torch.full(size=(self.act_dim,), fill_value = 0.5)
@@ -21,7 +26,10 @@ class PPO:
     def _init_hyperparameters(self):
         self.timesteps_per_batch = 4800
         self.max_timesteps_per_episode = 1600
+        self.lr = 0.005
         self.gamma = 0.95
+        self.n_updates_per_iteration = 5                          #Number of epochs, arbitrarily chosen
+        self.clip = 0.2                                           
 
     def compute_rtgs(self, batch_rews):
         batch_rtgs = []
@@ -86,9 +94,12 @@ class PPO:
 
         return batch_obs, batch_acts, batch_log_probs, batch_rtgs, batch_lens
 
-    def evaluate(self, batch_obs):
+    def evaluate(self, batch_obs, batch_acts):
         V = self.critic.forward(batch_obs).squeeze()
-        return V
+        mean = self.actor(batch_obs)
+        dist = MultivariateNormal(mean, self.cov_mat)
+        log_probs = dist.log_prob(batch_acts)
+        return V, log_probs
 
     def get_action(self, obs):
         #Calcula accion tomada por el actor, se llama mean porque es la media de nuestra distribucion (para efectos de exploracion)
@@ -121,7 +132,7 @@ class PPO:
 
         batch_obs, batch_acts, batch_log_probs, batch_rtgs, batch_lens = self.rollout()         #ALGO step 3
 
-        V = self.evaluate(batch_obs)                                                            #ALGO step 5
+        V, _ = self.evaluate(batch_obs)                                                         #ALGO step 5
 
         #Calculate A = Q - V_{phi_k}
         A_k = self.batch_rtgs - V.detach()                                                      #ALGO step 5
@@ -129,10 +140,23 @@ class PPO:
         #Normalize Advantages
         A_k = (A_k - A_k.mean())/(A_k.std() + 1e-10)                                            #Training trick
 
-        
+        for _ in range(self.n_updates_per_iteration):                                           #ALGO step 6
+            #Calculate pi_theta(a_t|s_t) and ratio with pi_theta old
+            _, current_log_probs = self.evaluate(batch_obs, batch_acts)
+            ratios = torch.exp(current_log_probs - batch_log_probs)
+
+            #Calculate surrogate losses 
+            surr1 = ratios*A_k
+            surr2 = torch.clamp(ratios, 1 - self.clip, 1+ self.clip)*A_k
+            actor_loss = (-torch.min(surr1,surr2)).mean()
+
+            #Calculate gradients and perform backpropagation for actor network
+            self.actor_optim.zero_grad()
+            actor_loss.backward()
+            self.actor_optim.step()                                                             #ALGO step 6
 
 
 
 
-
+    
             
